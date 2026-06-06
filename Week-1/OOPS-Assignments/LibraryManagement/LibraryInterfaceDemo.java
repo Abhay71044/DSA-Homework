251467b8150f5d1a6f913a76cbd79bea63d2c042
Abhay <abhaysingh71044@gmail.com>
2026-06-06 23:30:43 +0530

    Completed Week 1 Day 4

---
+++ Week-1/OOPS-Assignments/LibraryManagement/LibraryInterfaceDemo.java
@@ -0,0 +1,47 @@
+public class LibraryInterfaceDemo {
+
+    public static void main(String[] args) {
+
+        System.out.println("===== Kid User Tests =====");
+
+        KidUser kid = new KidUser();
+
+        // Age = 10
+        kid.age = 10;
+        kid.registerAccount();
+
+        // Age = 18
+        kid.age = 18;
+        kid.registerAccount();
+
+        // Book = Kids
+        kid.bookType = "Kids";
+        kid.requestBook();
+
+        // Book = Fiction
+        kid.bookType = "Fiction";
+        kid.requestBook();
+
+        System.out.println();
+
+        System.out.println("===== Adult User Tests =====");
+
+        AdultUser adult = new AdultUser();
+
+        // Age = 5
+        adult.age = 5;
+        adult.registerAccount();
+
+        // Age = 23
+        adult.age = 23;
+        adult.registerAccount();
+
+        // Book = Kids
+        adult.bookType = "Kids";
+        adult.requestBook();
+
+        // Book = Fiction
+        adult.bookType = "Fiction";
+        adult.requestBook();
+    }
+}
---
+++ Week-1/OOPS-Assignments/LibraryManagement/Readme.md
@@ -0,0 +1,20 @@
+# Library Management System using Interface
+
+## Objective
+To implement Interface in Java using different types of library users.
+
+## Classes Used
+1. LibraryUser (Interface)
+2. KidUser
+3. AdultUser
+4. LibraryInterfaceDemo
+
+## Concepts Used
+- Interface
+- Class and Objects
+- Method Overriding
+- Implements Keyword
+- Polymorphism
+
+## Author
+Abhay
